1c777c92dad54f751d89820170dff3411365d62e
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2021-06-22 20:03:38 +0100

    update pubsub

--- pubSubHandlers/clanCacheRefresh.py
+++ pubSubHandlers/clanCacheRefresh.py
@@ -4,10 +4,10 @@ from objects import glob
 class handler(generalPubSubHandler.generalPubSubHandler):
 	def __init__(self):
 		super().__init__()
-		self.structure = {"user_id": 999}
+		self.structure = ""
 		self.strict = False
 
 	def handle(self, data):
 		data = super().parseData(data)
 		if data is None: return
-		glob.clan_cache.cache_individual(data["user_id"])
+		glob.clan_cache.cache_individual(int(data))
